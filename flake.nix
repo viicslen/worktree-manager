ce823016acf19486cf1fe6641a4821bc0723dc81
Victor R <39545521+viicslen@users.noreply.github.com>
2025-11-24 11:01:08 -0500

    Add initial flake and package definitions for Worktree Manager

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Worktree Manager - A CLI tool for managing git worktrees";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.callPackage ./package.nix { };
+          wtm = pkgs.callPackage ./package.nix { };
+        };
+
+        apps = {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/wtm";
+          };
+          wtm = {
+            type = "app";
+            program = "${self.packages.${system}.wtm}/bin/wtm";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_23
+            gotools
+            gopls
+            go-outline
+            gocode
+            gopkgs
+            gocode-gomod
+            godef
+            golint
+          ];
+
+          shellHook = ''
+            echo "Worktree Manager development environment"
+            echo "Go version: $(go version)"
+          '';
+        };
+      }
+    );
+}
---
+++ package.nix
@@ -0,0 +1,31 @@
+{ lib
+, buildGoModule
+, git
+}:
+
+buildGoModule rec {
+  pname = "wtm";
+  version = "0.1.0";
+
+  src = ./.;
+
+  vendorHash = "sha256-pprnK2JKmPuR3Q+F8+vMDEdowlb3oX4BOOzW8NGOqgs=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+
+  # Ensure git is available at runtime
+  nativeBuildInputs = [ git ];
+  propagatedBuildInputs = [ git ];
+
+  meta = with lib; {
+    description = "Worktree Manager - A CLI tool for managing git worktrees";
+    homepage = "https://github.com/viicslen/worktrees";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "wtm";
+  };
+}
